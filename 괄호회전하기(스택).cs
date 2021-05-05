9d01c15f12d3a603383ce315bb3b52808806f139
Shin-Yong-Jae <sms3284@naver.com>
2021-05-05 22:21:48 +0900

    프로그래머스-괄호회전(스택)

--- 괄호회전하기(스택).cs
+++ 괄호회전하기(스택).cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 public class Solution {
     public static char[] CircularShiftLeft(char[] arr, int shifts)
     {
@@ -15,6 +16,7 @@ public class Solution {
         int resultcount =0;
         int rightopen =0;
         int leftopen =0;
+
         for(int i=0; i<temp.Length; i++)
         {
             if(temp[i]=='['||temp[i]=='{'||temp[i]=='(') rightopen++;
@@ -23,16 +25,18 @@ public class Solution {
         //괄호 개수가 안맞는 경우.
         if(rightopen != leftopen) return 0;
         
-        //괄호가 개수가 맞는 경우.
+        //괄호가 올바르지 못할경우.
         else
         {
             while(resultcount<s.Length)
             {
                 List<int> Count = new List<int>(){0,0,0,0,0,0};
+                List<Char> stack = new List<Char>();
                 for(int i=0; i<temp.Length; i++)
                 {
                     if(temp[i] == '[')
                     {
+                        stack.Add(temp[i]);
                         Count[0]++;
                     }
                     else if(temp[i] == ']')
@@ -44,15 +48,36 @@ public class Solution {
                             temp =CircularShiftLeft(temp,1);
                             break;
                         }
+                        if(stack.Count != 0 &&stack[stack.Count-1]=='[')
+                        {
+                            stack.RemoveAt(stack.Count-1);
+                        }
+                        else
+                        {
+                            resultcount++;
+                            temp =CircularShiftLeft(temp,1);
+                            break;
+                        }
                     }
                     else if(temp[i] == '{')
                     {
+                        stack.Add(temp[i]);
                         Count[2]++;
                     }
                     else if(temp[i] == '}')
                     {
                         Count[3]++;
-                        if(Count[2]<Count[3]) 
+                        if(Count[2]<Count[3] )  
+                        {
+                            resultcount++;
+                            temp =CircularShiftLeft(temp,1);
+                            break;
+                        }
+                        if(stack.Count != 0 &&stack[stack.Count-1]=='{')
+                        {
+                            stack.RemoveAt(stack.Count-1);
+                        }
+                        else
                         {
                             resultcount++;
                             temp =CircularShiftLeft(temp,1);
@@ -61,12 +86,23 @@ public class Solution {
                     }
                     else if(temp[i] == '(')
                     {
+                        stack.Add(temp[i]);
                         Count[4]++;
                     }
                     else if(temp[i] == ')')
                     {
                         Count[5]++;
-                        if(Count[4]<Count[5]) 
+                        if(Count[4]<Count[5] ) 
+                        {
+                            resultcount++;
+                            temp =CircularShiftLeft(temp,1);
+                            break;
+                        }
+                        if(stack.Count != 0 &&stack[stack.Count-1]=='(')
+                        {
+                            stack.RemoveAt(stack.Count-1);
+                        }
+                        else
                         {
                             resultcount++;
                             temp =CircularShiftLeft(temp,1);
@@ -78,7 +114,6 @@ public class Solution {
                         answer++;
                         resultcount++;
                         temp =CircularShiftLeft(temp,1);
-                        //break;
                     }
                 }
             }
