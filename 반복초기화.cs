8071e98a41b2056e2c7ee2a1bcb01a4a0da70b98
Shin-Yong-Jae <sms3284@naver.com>
2021-06-20 21:46:51 +0900

    반복초기화(정리)

---
+++ 반복초기화.cs
@@ -0,0 +1,25 @@
+//배열
+using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Solution {
+    public int solution(int n, int[,] computers) {
+        int answer = 1;
+        bool[] visited = Enumerable.Repeat<bool>(false,n).ToArray<bool>();
+        return answer;
+    }
+}
+ 
+
+//리스트
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Solution {
+    public int solution(int n, int[,] computers) {
+        int answer = 1;
+        List<bool> visited =  Enumerable.Repeat<bool>(false,n).ToList<bool>();
+        return answer;
+    }
+}
